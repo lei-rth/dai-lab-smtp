1e3aec58f776e90c0de25a1c29e4a74e01313ee9
Anthony Ledda <anthony.ledda@heig-vd.ch>
2024-11-11 15:44:01 +0100

    initialize SmtpPranker.java

--- smtppranker/src/main/java/ch/heig/dai/lab/smtppranker/Main.java
+++ smtppranker/src/main/java/ch/heig/dai/lab/smtppranker/Main.java
@@ -2,6 +2,11 @@ package ch.heig.dai.lab.smtppranker;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length != 3) {
+            System.out.println("Usage: java -jar smtppranker.jar <victims list> <messages list> <number of groups>");
+            System.exit(1);
+        }
+
+        SmtpPranker pranker = new SmtpPranker(args[0], args[1], Integer.parseInt(args[2]));
     }
 }
--- smtppranker/src/main/java/ch/heig/dai/lab/smtppranker/SmtpPranker.java
+++ smtppranker/src/main/java/ch/heig/dai/lab/smtppranker/SmtpPranker.java
@@ -1,5 +1,13 @@
 package ch.heig.dai.lab.smtppranker;
 
 public class SmtpPranker {
+    private String victims;
+    private String messages;
+    private int groups;
 
+    public SmtpPranker(String victims, String messages, int groups) {
+        this.victims = victims;
+        this.messages = messages;
+        this.groups = groups;
+    }
 }
